c5b4b272ea88445559f8536b2aa5818c89b3d404
seungho.ryu <98koom@hanmail.net>
2018-03-16 00:11:43 +0900

    We find .html files automatically.

--- vue_hello.py
+++ vue_hello.py
@@ -2,25 +2,28 @@ from flask import abort
 from flask import Flask
 from flask import render_template
 
+import os
+
 app = Flask(__name__, static_url_path='', static_folder='static')
 
-files = [
-    '1_vue_computed_watch',
-    '2_class_style_binding',
-    '3_condition_rendering',
-    '4_list_rendering',
-    '5_event_handling',
-    '6_form_binding',
-    '7_component',
-    '8_transitions',
-]
+
+def sort_files(x, y):
+    xx = x.split('_')[0]
+    yy = y.split('_')[0]
+    return int(xx) - int(yy)
 
 
 @app.route('/', methods=['GET'])
 def view_dashboard():
     try:
         links = []
-        for f in files:
+        file_names = []
+        file_list = os.listdir(os.path.dirname(os.path.abspath(__file__)) + '/static')
+        for ff in file_list:
+            if len(ff) > 4 and ff[-4:] == 'html':
+                file_names.append(ff)
+        file_names = sorted(file_names, cmp=sort_files)
+        for f in file_names:
             links.append({'href': '%s.html' % f, 'text': f})
         return render_template('index.html', links=links)
     except Exception, e:
